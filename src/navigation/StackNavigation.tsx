4a55c1b2d9282b1cbbe7a5ec0de2d042b5307712
Juan Camilo <juancamilomendozavillegas14@gmail.com>
2024-11-07 18:57:27 -0500

    feat: initialize proyect pokedex named "DexGo"

---
+++ src/navigation/StackNavigation.tsx
@@ -0,0 +1,22 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from '../screens/Home/HomeScreen';
+import { PokemonScreen } from '../screens/Pokemon/PokemonScreen';
+import { SearchScreen } from '../screens/Search/SearchScreen';
+
+export type RootStackParams = {
+    Home: undefined,
+    Pokemon: { pokemonId: number };
+    Search: undefined;
+}
+
+const Stack = createStackNavigator<RootStackParams>();
+
+export const StackNavigation = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Pokemon" component={PokemonScreen} />
+      <Stack.Screen name="Search" component={SearchScreen} />
+    </Stack.Navigator>
+  );
+}
