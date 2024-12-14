c5ad79823275060bfbe749c9cfdcc7d7e0ce9b7a
Juan Camilo <juancamilomendozavillegas14@gmail.com>
2024-12-14 08:26:47 -0500

    feat(homescreen): add loader for more content

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -2,12 +2,13 @@ import { FlatList, StyleSheet, View } from "react-native";
 import React from "react";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { PokemonBg } from "@/components/ui/PokemonBg";
-import { Text } from "react-native-paper";
+import { ActivityIndicator, Text } from "react-native-paper";
 import { globalTheme } from "@/config/theme/Globa-theme";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { PokemonCard } from "@/components/pokemons/PokemonCard";
 import { getPokemons } from "@/actions/Pokemons";
 import { FullScreenLoader } from "@/components/ui/FullScreenLoader";
+import LottieView from "lottie-react-native";
 
 export const HomeScreen = () => {
   const { top } = useSafeAreaInsets()
@@ -20,7 +21,7 @@ export const HomeScreen = () => {
     staleTime: 1000 * 60 * 60, //60 minutos
   });
 
-  if ( isLoading ) {
+  if (isLoading) {
     return <FullScreenLoader />
   }
 
@@ -40,10 +41,30 @@ export const HomeScreen = () => {
         showsVerticalScrollIndicator={false}
         ListHeaderComponent={() => <Text variant="displayMedium">Pokedex</Text>}
         renderItem={({ item }) => <PokemonCard pokemon={item} />}
-        onEndReachedThreshold={0.8}
+        onEndReachedThreshold={0.6}
         onEndReached={() => fetchNextPage()}
         initialNumToRender={10}
         windowSize={5}
+        ListFooterComponent={() => (
+          <View style={{
+            height: 150,
+            marginBottom: 50,
+            justifyContent: "center",
+            backgroundColor: "rgba(255, 255, 255, 0.2)",
+            alignItems: "center",
+            borderRadius: 10,
+            padding: 10,
+          }}>
+            <LottieView
+              source={require('../../lotties/loading.json')}
+              autoPlay
+              style={{
+                width: 400,
+                height: 150,
+              }}
+              loop />
+          </View>
+        )}
       />
     </View>
   );
